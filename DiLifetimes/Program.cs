e734d49b0b54e0fafb78e0d159f44204bf5da0f5
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-04-23 16:54:13 +0600

    Add Three classes with DataTime Properties

--- DiLifetimes/Program.cs
+++ DiLifetimes/Program.cs
@@ -1,8 +1,30 @@
 var builder = WebApplication.CreateBuilder(args); // gives tool box to configure the app
 
+builder.Services.AddSingleton(x => new SingletonDate());
+
 
 var app = builder.Build(); // Takes Those configurations and constructs the app
 
 
+app.MapGet("/", () =>
+{
+
+});
+
 app.Run(); // Turns on the server so it can start accepting web traffic 
 
+
+public class SingletonDate
+{
+    public DateTime Date { get; set; } = DateTime.Now;
+}
+
+public class TransientDate
+{
+    public DateTime Date { get; set; } = DateTime.Now;
+}
+
+public class ScopedDate
+{
+    public DateTime Date { get; set; } = DateTime.Now;
+}
